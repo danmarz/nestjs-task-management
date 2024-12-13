665ded29f24018c33b97d0570bf44e2c072d92d9
dan m. <dm@danmarius.dev>
2024-12-13 23:06:37 +0100

    Update TypeOrmModule to use the dynamic (.env) NestJs configuration
    - Inject ConfigService into TypeOrm Config
    - Refactor typeOrmConfig into a Factory Function

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        typeOrmConfig(configService),
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+export const typeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
+  host: configService.get<string>('PG_HOST', 'localhost'),
+  port: configService.get<number>('PG_PORT', 5432),
+  username: configService.get<string>('PG_USERNAME'),
+  password: configService.get<string>('PG_PASSWORD'),
   database: 'taskmanagement',
   entities: [__dirname + '/../**/*.entity.ts'],
   synchronize: true,
-};
+});
