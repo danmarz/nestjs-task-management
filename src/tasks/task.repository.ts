068ae98be7e010497f02354756234174c708ff19
dan m. <dm@danmarius.dev>
2024-12-14 01:36:33 +0100

    Refactor complete: getTasks() method now uses Task repository to filter and retrieve tasks from Postgres

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,9 +2,31 @@ import { DataSource } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 export const TaskRepository = (dataSource: DataSource) =>
   dataSource.getRepository(Task).extend({
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+      const { status, search } = filterDto;
+      const query = this.createQueryBuilder('task');
+
+      // Apply status filter if provided
+      if (status) {
+        query.andWhere('task.status = :status', { status });
+      }
+
+      // Apply search filter if provided
+      if (search) {
+        query.andWhere(
+          '(task.title LIKE :search OR task.description LIKE :search)',
+          { search: `%${search}%` },
+        );
+      }
+
+      const tasks = await query.getMany();
+      return tasks;
+    },
+
     async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
       const { title, description } = createTaskDto;
 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,6 +4,7 @@ import { TaskRepository } from './task.repository';
 import { Task } from './task.entity';
 import { DataSource } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,23 +12,11 @@ export class TasksService {
   constructor(private readonly dataSource: DataSource) {
     this.taskRepository = TaskRepository(dataSource);
   }
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-  // getFilteredTasks(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       (task) =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-  //   return tasks;
-  // }
+
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
+  }
+
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne({ where: { id } });
     if (!found) {
