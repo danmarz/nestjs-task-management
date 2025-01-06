8525946d79c5045584686c4f6e4bbe4327707af3
dan m. <dm@danmarius.dev>
2025-01-06 20:22:52 +0100

    Add duplicate username error handling for signup flow

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,13 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import e from 'express';
 
 @Injectable()
 export class UserRepository {
@@ -14,7 +19,16 @@ export class UserRepository {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.userRepository.create({ username, password });
-    await this.userRepository.save(user);
+    try {
+      const user = this.userRepository.create({ username, password });
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
